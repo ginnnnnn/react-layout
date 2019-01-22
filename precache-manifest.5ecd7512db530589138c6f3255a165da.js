367c45a3358ea079b0ea454fd320bc0dff251931
ginnnnnn <aginlo@hotmail.com>
2019-01-22 21:14:42 +1030

    Updates

---
+++ precache-manifest.5ecd7512db530589138c6f3255a165da.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "911f1b96945b47721a40",
+    "url": "/ginnnnnn/react-layout.git/static/css/main.56b92ffc.chunk.css"
+  },
+  {
+    "revision": "911f1b96945b47721a40",
+    "url": "/ginnnnnn/react-layout.git/static/js/main.911f1b96.chunk.js"
+  },
+  {
+    "revision": "d6d9017c76c6dc1b0c0c",
+    "url": "/ginnnnnn/react-layout.git/static/js/1.d6d9017c.chunk.js"
+  },
+  {
+    "revision": "ec94045c59f179bea73c",
+    "url": "/ginnnnnn/react-layout.git/static/js/runtime~main.ec94045c.js"
+  },
+  {
+    "revision": "86b3e9841d7ea0db8b4e9dd131e1a87d",
+    "url": "/ginnnnnn/react-layout.git/static/media/files_2.86b3e984.png"
+  },
+  {
+    "revision": "051c6f6fe135bc8a4631e51bd81a3ad1",
+    "url": "/ginnnnnn/react-layout.git/static/media/data_storage_2_2.051c6f6f.png"
+  },
+  {
+    "revision": "5c3fe95bda9d17cb5229884cd6e67ede",
+    "url": "/ginnnnnn/react-layout.git/static/media/desktop_analytics_2.5c3fe95b.png"
+  },
+  {
+    "revision": "b8eaa6a4b113509bfd8b52109347631f",
+    "url": "/ginnnnnn/react-layout.git/static/media/monitor_coding_2.b8eaa6a4.png"
+  },
+  {
+    "revision": "47b54495f4153484ba4677f1a9cdba18",
+    "url": "/ginnnnnn/react-layout.git/static/media/monitor_settings_2.47b54495.png"
+  },
+  {
+    "revision": "b4d1df15a5a07644d64c22e78cf24b0e",
+    "url": "/ginnnnnn/react-layout.git/static/media/server_2_2.b4d1df15.png"
+  },
+  {
+    "revision": "11781973eaf050bf371dc0b26fd37afb",
+    "url": "/ginnnnnn/react-layout.git/static/media/server_3.11781973.png"
+  },
+  {
+    "revision": "cf303795d7acdf14e49cd4f8c72743f4",
+    "url": "/ginnnnnn/react-layout.git/static/media/server_safe_2.cf303795.png"
+  },
+  {
+    "revision": "9dc7b0989719120536932288a434104c",
+    "url": "/ginnnnnn/react-layout.git/static/media/undraw.9dc7b098.png"
+  },
+  {
+    "revision": "9a7e0ba9419e57eec041bac954becabf",
+    "url": "/ginnnnnn/react-layout.git/index.html"
+  }
+];
